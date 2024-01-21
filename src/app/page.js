283d60dd3e689c73b06de7ec8b9fa662ca799986
Abdulateef Abdulsalam <abdulsalamopeyemi030@gmail.com>
2024-01-21 23:07:52 +0100

    Menu sectin started

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Hero from "@/components/layout/Hero"
+import Menu from "@/components/layout/Menu"
 import Navbar from "@/components/layout/Navbar"
 export default function Home() {
   return (
   <>
   <Navbar/>
   <Hero/>
-  
+   <Menu/>
   </>
   )
 }
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -2,10 +2,14 @@ import Image from "next/image"
 import Right from "../icons/Right"
 export default function Hero() {
   return (
-    <section className="grid grid-cols-2">
-        <div className="py-8">
-            <h3 className="text-4xl font-semibold">Food is Life</h3>
-            <p className="my-4 text-gray-500">Food loses about 30% of its flavour when tasted in the sky.</p>
+    <section className="hero">
+        <div className="py-12">
+            <h3 className="text-4xl font-semibold">Food is&nbsp;
+            <span className="text-primary">
+                Life
+            </span>
+             </h3>
+            <p className="my-6 text-gray-500 text-sm">Food loses about 30% of its flavour when tasted in the sky.</p>
             <div className="flex gap-4 text-sm">
                 <button className="bg-primary uppercase text-white px-4 py-2 rounded-full flex items-center gap-2">
                     Order Now
